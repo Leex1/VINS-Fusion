5abaf8cd774fe0ab90992fab08cc70fe751b550b
ShenYuanzhe2022M <1286194374@qq.com>
2023-09-02 11:48:10 +0800

    suitable for noetic

--- vins_estimator/src/featureTracker/feature_tracker.cpp
+++ vins_estimator/src/featureTracker/feature_tracker.cpp
@@ -10,6 +10,9 @@
  *******************************************************/
 
 #include "feature_tracker.h"
+#include <opencv2/imgproc/types_c.h>
+
+
 
 bool FeatureTracker::inBorder(const cv::Point2f &pt)
 {
